a979b63f54a6ef980e188bad2aac87786791ae2a
Roger <rogercinhoz@gmail.com>
2024-07-31 19:29:18 -0300

    final final

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+// Me siento orgulloso de estas funciones, aunque son sencillas fue lo que más me gustó de hacer el portafolio
+
+function fondo1() {
+    document.getElementById('fondo').classList.remove('fondo2');
+    document.getElementById('fondo').classList.add('fondo1');
+}
+function fondo2() {
+    document.getElementById('fondo').classList.remove('fondo1');
+    document.getElementById('fondo').classList.add('fondo2');
+}
+
